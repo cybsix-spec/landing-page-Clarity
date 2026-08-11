73efd955c0e820da1e0a0fe299335ab86b142a55
cybsix-spec <amar.whello@gmail.com>
2026-08-11 10:27:52 +0700

    box-shadow

--- script.js
+++ script.js
@@ -2,9 +2,7 @@
 const hamburgerBtn = document.getElementById('hamburger-btn');
 const closeBtn = document.getElementById('close-btn');
 const mobileMenu = document.getElementById('mobile-menu');
-
 const mobileNavLinks = document.querySelectorAll('.mobile-nav-links a');
-
 
 hamburgerBtn.addEventListener('click', () => {
   mobileMenu.classList.add('active');
